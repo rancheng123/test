80e2c6c549881b6d85c3f1803a8753e8995863a9
邓铃波 <ran.cheng@mydreamplus.com>
2019-04-29 17:55:59 +0800

    代码回滚

--- a.js
+++ a.js
@@ -1,5 +1,22 @@
-var revert = '1'
-var revert = '2'
+var test = 'test reset hard start'
+var a = 1;
+var b = 2;
+var c = 3;
 
+var dev = '1'
+var dev = '2'
+var dev = '3'
+
+var dev = '4'
+var dev = '5'
+var dev = '6'
+
+var dev = '7'
+var dev = '8'
+var dev = '9'
+var dev = '10'
+var dev = '11'
+var dev = '12'
+var dev = '13'
 
 //sh ../../businessDir/operationShell/merge.sh
