e379d59113d74d22cb59c158d2f243ef0005f57f
邓铃波 <ran.cheng@mydreamplus.com>
2019-04-29 15:01:42 +0800

    revert 1

--- merge_qa.js
+++ merge_qa.js
@@ -1,10 +1,13 @@
-var exec = require('child_process').exec;
+var origin = 'dev';
+var target = 'qa';
 
+
+
+var exec = require('child_process').exec;
 var path = require('path');
 var shFile = path.resolve(__dirname,'../../businessDir/operationShell/merge.sh');
 
-
-exec('sh '+ shFile, function(err,stdout,stderr){
+exec('sh '+ shFile + ' '+ origin +' ' + target, function(err,stdout,stderr){
     if(err) {
         console.log('get weather api error:'+stderr);
     } else {
